34069e79b82f082adcf2eab00f132113c1064428
Sebastian <social@byhumans.works>
2019-01-31 18:26:19 -0300

    Create cards.js

---
+++ js/cards.js
@@ -0,0 +1,16 @@
+var search = document.querySelector('.search-fld');
+var searchVal = document.querySelector('.search-filter');
+var debounce;
+
+search.addEventListener('keyup', function() {
+  clearTimeout(debounce);
+  debounce = setTimeout(function() {
+    var value = search.value;
+    var searchTerm = '';
+    
+    if(value.length) searchTerm = '[data-tags*="' + search.value + '"]';
+    
+    searchVal.setAttribute('uk-filter-control', searchTerm);
+    searchVal.click();
+  }, 300);
+});
